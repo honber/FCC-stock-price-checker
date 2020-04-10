4804dae935e8cdb2327c3ddbb5751d5cb98c00d8
Glitch (hb-fcc-stock-price-checker) <none>
2020-04-10 15:09:58 +0000

    refactor - async/await and functions

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,16 @@
 2. When I type in input (testForm2) company symbol and I check like (testForm2), API cheks if if company is listed in Nasdaq (if it is not - returns information)
    If company is listed in Nasdaq and not stored in my DB - new record is stored in DB, and object is returned (likes: 1)
    If company is listed in Nasdaq and stored in my DB - API ckecks if IP of client, which sent a request is equal to any of IPs stored in likes Array: no -> adds client's IP to likes Array.
-   
-
-
-
-
 */
+
+
+/*      const test = new stockModel({stock: 'GOOG', likes: ['1.1.1.1', '2.2.2.2']});
+        test.save((err, res) => {
+          if (err){console.log(err)}
+          else{console.log('OK');}
+        }) 
+*/
+
 
 //http://eoddata.com/stocklist/NASDAQ.htm
 
@@ -25,6 +29,46 @@ const stockDataHandler     = require('../controllers/stockHandler.js');
 
 const CONNECTION_STRING = process.env.MLAB_URI;
 
+function companyIsListedInNasdaq(data) {
+  const isNotListed = ['Unknown symbol', 'Invalid symbol'];
+  return isNotListed.includes(data) ? false: true;
+}
+
+async function prepareOneStockData(stockName) {
+  const stockData = await getNasdaqData(stockName);
+  if (!companyIsListedInNasdaq(stockData)) { return 'Company is not listed on NASDAQ.'; } 
+  const record = new stockDataHandler(stockData.symbol, stockData.latestPrice);
+  const stockDataFromDB = await stockModel.findOne({stock: record.stock}, (error, response) =>{
+    if (error) { console.log(error.message); }
+    return response
+  });
+  return stockDataFromDB !== null 
+  ? 
+  ({stockData: {...record, likes: stockDataFromDB.likes.length}}) 
+  : 
+  ({stockData: {...record, likes: 0}}) 
+}
+
+async function prepareOneStockDataWhenLikeIsChecked(stockName) {
+}
+
+async function prepareStockDataOneOfTwo(stockName) {
+  let stockLikesCount = 0;
+  let record = {};  
+  let stockData = await getNasdaqData(stockName);
+    if (companyIsListedInNasdaq(stockData)) { 
+      record = new stockDataHandler(stockData.symbol, stockData.latestPrice); 
+      const stockDataFromDB = await stockModel.findOne({stock: record.stock}, (error, response) => {
+        if (error) { console.log(error.message) }
+        return response
+      })
+      stockDataFromDB === null ? stockLikesCount = 0 : stockLikesCount = stockDataFromDB.likes.length;    
+    }
+    record.likes = stockLikesCount;
+    return record;
+}
+
+
 const connectOptions = { 
   useFindAndModify: false,
   useNewUrlParser: true,
@@ -39,19 +83,6 @@ mongoose.connect(CONNECTION_STRING, connectOptions, err => {
     console.log('Connection to database succesful'); 
   }
 });
-
-/*      const test = new stockModel({stock: 'GOOG', likes: ['1.1.1.1', '2.2.2.2']});
-        test.save((err, res) => {
-          if (err){console.log(err)}
-          else{console.log('OK');}
-        }) */
-
-
-function companyIsListedInNasdaq(data) {
-  const isNotListed = ['Unknown symbol', 'Invalid symbol'];
-  return isNotListed.includes(data) ? false: true;
-  
-}
 
 
 module.exports = function (app) {
@@ -69,7 +100,7 @@ module.exports = function (app) {
         
         const clientIp = req.clientIp;
         console.log(`Client's IP: ${clientIp}`);
-        
+
         const stockData = await getNasdaqData(stock);
         if (!companyIsListedInNasdaq(stockData)) { return res.send('Company is not listed on NASDAQ.'); } 
         const record = new stockDataHandler(stockData.symbol, stockData.latestPrice);
@@ -77,7 +108,7 @@ module.exports = function (app) {
             if (error) { res.send(error.message); }
             return response;
         });
-        
+      
         // company is listed in Nasdaq but not stored in my DB 
         if (stockDataFromDB === null) { 
           const stockToStoreInDB = new stockModel({
@@ -102,69 +133,29 @@ module.exports = function (app) {
          
     
       else if (oneStockInRequest && !like) {   
-        const stockData = await getNasdaqData(stock);
-        if (!companyIsListedInNasdaq(stockData)) { return res.send('Company is not listed on NASDAQ.'); } 
-        const record = new stockDataHandler(stockData.symbol, stockData.latestPrice);
-        const stockDataFromDB = await stockModel.findOne({stock: record.stock}, (error, response) =>{
-            if (error) { res.send(error.message) }
-            return response
-        });
-        return stockDataFromDB !== null 
-          ? 
-          res.json({stockData: {...record, likes: stockDataFromDB.likes.length}}) 
-          : 
-          res.json({stockData: {...record, likes: 0}}) 
+        const record = await prepareOneStockData(stock);
+        res.json(record);
       }
-    
     
       else if (twoStocksInRequest && like) {
         res.send('2 strings and like')
       }
     
-    
       else if(twoStocksInRequest && !like) {
-        let record1, record2 = {};
-        let stock1LikesCount, stock2LikesCount;
-        let relLikes1, relLikes2;
-        let result1, result2;
         const stock1 = stock[0];
         const stock2 = stock[1];
+        const record1 = await prepareStockDataOneOfTwo(stock1);
+        const record2 = await prepareStockDataOneOfTwo(stock2);
         
-        let stock1Data = await getNasdaqData(stock1);
-        if (!companyIsListedInNasdaq(stock1Data)) {  stock1LikesCount = 0 }
-        else { 
-          record1 = new stockDataHandler(stock1Data.symbol, stock1Data.latestPrice); 
-          const stock1DataFromDB = await stockModel.findOne({stock: record1.stock}, (error, response) => {
-            if (error) { res.send(error.message) }
-            return response
-          })
-          stock1DataFromDB === null ? stock1LikesCount = 0 : stock1LikesCount = stock1DataFromDB.likes.length;
-        }
-              
-        let stock2Data = await getNasdaqData(stock2);
-        if (!companyIsListedInNasdaq(stock2Data)) { stock2LikesCount = 0 } 
-        else {
-          record2 = new stockDataHandler(stock2Data.symbol, stock2Data.latestPrice);   
-          const stock2DataFromDB = await stockModel.findOne({stock: record2.stock}, (error, response) => {
-            if (error) { res.send(error.message) }
-            return response
-          })
-          stock2DataFromDB === null ? stock2LikesCount = 0 : stock2LikesCount = stock2DataFromDB.likes.length;          
-        }
+        const relLikes1 = record1.likes - record2.likes;
+        const relLikes2 = record2.likes - record1.likes;
+        record1.rel_likes = relLikes1;
+        record2.rel_likes = relLikes2;
+        delete record1.likes;
+        delete record2.likes;
         
-        
-        relLikes1 = stock1LikesCount - stock2LikesCount;
-        relLikes2 = stock2LikesCount - stock1LikesCount;
-        
-        result1 = {...record1, rel_likes: relLikes1}
-        result2 = {...record2, rel_likes: relLikes2}
-        
-        res.json({stockData: [result1, result2]});
-
+        res.json({stockData: [record1, record2]});        
       }
-    
-    
-    
     
       else {
         res.json({"stockData":{"likes":0}})
